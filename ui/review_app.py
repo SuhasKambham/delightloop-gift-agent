2a5a3c21a1365f34bc346780c319f76ae30df099
Suhas Kambham <32142387+SuhasKambham@users.noreply.github.com>
2026-06-25 23:19:26 +0530

    Update review_app.py
    added backend api_url

--- ui/review_app.py
+++ ui/review_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import json
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "API_URL = "https://delightloop-gift-agent.onrender.com""
 
 st.set_page_config(
     page_title="DelightLoop Gift Agent",
